5c0da70aed4bde9e10f559eddcc3dc883c2f7653
Albert Zolile Mbalula <zolilembalula@gmail.com>
2025-11-27 13:52:23 +0200

    Add Wouter Router base to main.jsx for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './styles.css'
+import { Router } from 'wouter'
 
-createRoot(document.getElementById('root')).render(<App />)
+// IMPORTANT for GitHub Pages:
+const base = "/submission-site"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router base={base}>
+      <App />
+    </Router>
+  </React.StrictMode>,
+)
